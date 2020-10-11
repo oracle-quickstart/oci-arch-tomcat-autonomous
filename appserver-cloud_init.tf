0fe4d1b196e2c219025eacf03dfa6c17e960ac55
Emmanuel Leroy <emmanuel.leroy@oracle.com>
2020-10-11 14:16:37 -0700

    fix warning about interpolation

--- appserver-cloud_init.tf
+++ appserver-cloud_init.tf
@@ -2,9 +2,9 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 data "template_file" "key_script" {
-  template = "${file("./templates/sshkey.tpl")}"
+  template = file("./templates/sshkey.tpl")
   vars = {
-    "ssh_public_key" = "${tls_private_key.opc_key.public_key_openssh}"
+    "ssh_public_key" = tls_private_key.opc_key.public_key_openssh
   }
 }
 
@@ -17,7 +17,7 @@ data "template_cloudinit_config" "app_server_cloud_init" {
   part {
     filename     = "ainit.sh"
     content_type = "text/x-shellscript"
-    content      = "${data.template_file.key_script.rendered}"
+    content      = data.template_file.key_script.rendered
   }
 
 }
