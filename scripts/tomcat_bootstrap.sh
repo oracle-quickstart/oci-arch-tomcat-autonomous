62d245bc2dbb02a5b8b9b37b0262244dae3b9e4d
Lukasz Feldman <lukasz.feldman@oracle.com>
2021-05-26 08:28:44 +0200

    Updated scripts/tomcat_bootstrap.sh

--- scripts/tomcat_bootstrap.sh
+++ scripts/tomcat_bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Install Java JDK & Tomcat
+echo '== 1. Install Java JDK & Tomcat'
 yum install -y jdk
 
 useradd tomcat
@@ -13,23 +14,41 @@ chmod +x /u01/apache-tomcat-${tomcat_version}/bin/*.sh
 
 # Install Oracle instant client
 
-echo '== 1. Install Oracle instant client'
+echo '== 2. Install Oracle instant client'
 if [[ $(uname -r | sed 's/^.*\(el[0-9]\+\).*$/\1/') == "el8" ]]
 then 
-  yum install -y https://yum.oracle.com/repo/OracleLinux/OL8/oracle/instantclient/aarch64/getPackage/oracle-instantclient19.10-basic-19.10.0.0.0-1.aarch64.rpm
-  yum install -y https://yum.oracle.com/repo/OracleLinux/OL8/oracle/instantclient/aarch64/getPackage/oracle-instantclient19.10-sqlplus-19.10.0.0.0-1.aarch64.rpm
+  if [[ $(uname -m | sed 's/^.*\(el[0-9]\+\).*$/\1/') == "aarch64" ]]
+  then
+    echo '=== 2.1 aarch64 platform & OL8' 
+  	yum install -y https://yum.oracle.com/repo/OracleLinux/OL8/oracle/instantclient/aarch64/getPackage/oracle-instantclient19.10-basic-19.10.0.0.0-1.aarch64.rpm
+  	yum install -y https://yum.oracle.com/repo/OracleLinux/OL8/oracle/instantclient/aarch64/getPackage/oracle-instantclient19.10-sqlplus-19.10.0.0.0-1.aarch64.rpm
+  else
+  	echo '=== 2.1 x86_64 platform & OL8'
+    dnf install -y oracle-instantclient-release-el8
+    dnf install -y oracle-instantclient-basic
+  fi	
 else
-  yum install -y https://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/aarch64/getPackage/oracle-instantclient19.10-basic-19.10.0.0.0-1.aarch64.rpm
-  yum install -y https://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/aarch64/getPackage/oracle-instantclient19.10-sqlplus-19.10.0.0.0-1.aarch64.rpm
+  if [[ $(uname -m | sed 's/^.*\(el[0-9]\+\).*$/\1/') == "aarch64" ]]
+  then 	
+  	echo '=== 2.1 aarch64 platform & OL7' 
+  	yum install -y https://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/aarch64/getPackage/oracle-instantclient19.10-basic-19.10.0.0.0-1.aarch64.rpm
+    yum install -y https://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/aarch64/getPackage/oracle-instantclient19.10-sqlplus-19.10.0.0.0-1.aarch64.rpm
+  else
+    echo '=== 2.1 x86_64 platform & OL7'
+    yum install -y oracle-instantclient-release-el7
+    yum install -y oracle-instantclient-basic
+  fi
 fi 
 
 # Prepare TDE wallet 
+echo '== 3. Prepare TDE wallet'
 unzip -o /tmp/${tde_wallet_zip_file} -d /usr/lib/oracle/${oracle_instant_client_version_short}/client64/lib/network/admin/
 
 export tomcat_host='${tomcat_host}'
 
 if [[ $tomcat_host == "tomcat-server-0" ]]; then
 	# Create TODOAPP user in ATP
+	echo '== 4. Create TODOAPP user in ATP'
 	rm -rf /tmp/create_todoapp_user.sql
 	echo 'CREATE USER ${db_user_name} IDENTIFIED BY "${db_user_password}";' | sudo tee -a /tmp/create_todoapp_user.sql
 	echo 'GRANT CREATE SESSION TO ${db_user_name};' | sudo tee -a /tmp/create_todoapp_user.sql
@@ -41,6 +60,7 @@ if [[ $tomcat_host == "tomcat-server-0" ]]; then
 	/usr/lib/oracle/${oracle_instant_client_version_short}/client64/bin/sqlplus admin/${db_user_password}@${db_name}_medium @/tmp/create_todoapp_user.sql > /tmp/create_todoapp_user.log
 	cat /tmp/create_todoapp_user.log
 	# Create TODOS table in ATP
+	echo '== 5. Create TODOS table in ATP'
 	rm -rf /tmp/create_todos_table.sql
 	echo 'CREATE TABLE TODOS (' | sudo tee -a /tmp/create_todos_table.sql
 	echo 'id number GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),' | sudo tee -a /tmp/create_todos_table.sql 
@@ -56,14 +76,17 @@ if [[ $tomcat_host == "tomcat-server-0" ]]; then
 fi
 
 # Prepare application and Tomcat
+echo '== 6. Prepare application and Tomcat'
 cp /home/opc/context.xml /u01/apache-tomcat-${tomcat_version}/conf/context.xml
 
 # Start Tomcat
+echo '== 7. Start Tomcat'
 setsebool -P tomcat_can_network_connect_db 1
 chmod +x /u01/apache-tomcat-${tomcat_version}/bin/*.sh
 sudo -u tomcat nohup /u01/apache-tomcat-${tomcat_version}/bin/startup.sh &
 
 # Download TODOAPP and deploy in Tomcat
+echo '== 8. Download TODOAPP and deploy in Tomcat'
 wget -O /home/opc/todoapp.war https://github.com/oracle-quickstart/oci-arch-tomcat-autonomous/releases/latest/download/todoapp-atp.war
 chown opc:opc /home/opc/todoapp.war
 cp /home/opc/todoapp.war /u01/apache-tomcat-${tomcat_version}/webapps
@@ -71,6 +94,7 @@ chown tomcat:tomcat /u01/apache-tomcat-${tomcat_version}/webapps/todoapp.war
 sleep 30
 
 # Adding Tomcat as systemd service
+echo '== 9. Adding Tomcat as systemd service'
 cp /home/opc/tomcat.service /etc/systemd/system/
 ls -latr /etc/systemd/system/tomcat.service
 chown root:root /etc/systemd/system/tomcat.service
@@ -82,6 +106,7 @@ sleep 20
 ps -ef | grep tomcat
 
 # Stop and disable firewalld 
+echo '== 10. Stop and disable firewalld '
 service firewalld stop
 systemctl disable firewalld
 
